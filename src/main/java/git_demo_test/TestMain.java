086fc3245492b72e304c4855625886d5e09522b7
hongtaolong <hongtaolong1030@163.com>
2019-04-12 11:25:36 +0800

    testv3

--- src/main/java/git_demo_test/TestMain.java
+++ src/main/java/git_demo_test/TestMain.java
@@ -3,6 +3,6 @@ package git_demo_test;
 public class TestMain {
 
 	public static void main(String[] args) {
-		System.out.println("git test test A .");
+		System.out.println("git test test");
 	}
 }
