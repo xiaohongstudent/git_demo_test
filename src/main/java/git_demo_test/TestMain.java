6d25d157d5ab7afde3ba950663d5102d8974b0dc
hongtaolong <hongtaolong1030@163.com>
2019-04-12 16:04:38 +0800

    modify

--- src/main/java/git_demo_test/TestMain.java
+++ src/main/java/git_demo_test/TestMain.java
@@ -3,6 +3,6 @@ package git_demo_test;
 public class TestMain {
 
 	public static void main(String[] args) {
-		System.out.println("git taolong test merge master branch update");
+		System.out.println("git taolong test revert");
 	}
 }
